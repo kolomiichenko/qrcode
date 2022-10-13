bb13a621f57b50e0f10a8f167a552633aea62107
Andrii Kolomiichenko <bboywilld@gmail.com>
2022-10-13 16:26:45 +0700

    Add change title color

--- qrcode.go
+++ qrcode.go
@@ -2,80 +2,80 @@ package qrcode
 
 import (
 	"bytes"
-	"github.com/golang/freetype"
-	"github.com/golang/freetype/truetype"
-	"github.com/lucasb-eyer/go-colorful"
-	"github.com/nfnt/resize"
-	"github.com/skip2/go-qrcode"
 	"image"
 	"image/color"
 	"image/draw"
 	"image/png"
 	"unicode/utf8"
+
+	"github.com/golang/freetype"
+	"github.com/golang/freetype/truetype"
+	"github.com/lucasb-eyer/go-colorful"
+	"github.com/nfnt/resize"
+	"github.com/skip2/go-qrcode"
 )
 
 type QrCode struct {
-	Font *truetype.Font  //字体文件
-	LogoImage image.Image  //logo文件
-	Size  int       // 生成的图片宽度
-	FontSize float64    //字号
-	FontDPI  float64  //字体密度
-	FrontColor color.Color  //字体颜色
-	BackColor  color.Color   //背景颜色
+	Font       *truetype.Font //字体文件
+	LogoImage  image.Image    //logo文件
+	Size       int            // 生成的图片宽度
+	FontSize   float64        //字号
+	FontDPI    float64        //字体密度
+	FrontColor color.Color    //字体颜色
+	BackColor  color.Color    //背景颜色
 }
 
-func Init(fontByte,logoByte []byte, size int,fontSize float64,fontDPI float64 ,frontColor string,backColor string)(qrCode *QrCode,err error){
-	qrCode=new(QrCode)
-	qrCode.Size=size
-	qrCode.FontSize=fontSize
-	qrCode.FontDPI=fontDPI
+func Init(fontByte, logoByte []byte, size int, fontSize float64, fontDPI float64, frontColor string, backColor string) (qrCode *QrCode, err error) {
+	qrCode = new(QrCode)
+	qrCode.Size = size
+	qrCode.FontSize = fontSize
+	qrCode.FontDPI = fontDPI
 
-	if frontColor!=""{
+	if frontColor != "" {
 		qrCode.FrontColor, err = colorful.Hex(frontColor)
-		if err!=nil{
+		if err != nil {
 			return
 		}
-	}else{
-		qrCode.FrontColor=color.Black
+	} else {
+		qrCode.FrontColor = color.Black
 	}
 
-	if frontColor!=""{
+	if frontColor != "" {
 		qrCode.BackColor, err = colorful.Hex(backColor)
-		if err!=nil{
+		if err != nil {
 			return
 		}
-	}else{
-		qrCode.BackColor=color.White
+	} else {
+		qrCode.BackColor = color.White
 	}
-	if size!=0{
-		qrCode.Size=size
-	}else{
-		qrCode.Size=1000
-	}
-
-	if fontSize>1{
-		qrCode.FontSize=fontSize
-	}else{
-		qrCode.FontSize=40
+	if size != 0 {
+		qrCode.Size = size
+	} else {
+		qrCode.Size = 1000
 	}
 
-	if fontDPI>1{
-		qrCode.FontDPI=fontDPI
-	}else{
-		qrCode.FontDPI=72
+	if fontSize > 1 {
+		qrCode.FontSize = fontSize
+	} else {
+		qrCode.FontSize = 40
 	}
 
+	if fontDPI > 1 {
+		qrCode.FontDPI = fontDPI
+	} else {
+		qrCode.FontDPI = 72
+	}
 
-	if fontByte!=nil{
-		err=qrCode.readFontFromByte(fontByte)
-		if err!=nil{
+	if fontByte != nil {
+		err = qrCode.readFontFromByte(fontByte)
+		if err != nil {
 			return
 		}
 	}
 
-	if logoByte!=nil{
-		err=qrCode.readLogoImageFromByte(logoByte)
-		if err!=nil{
+	if logoByte != nil {
+		err = qrCode.readLogoImageFromByte(logoByte)
+		if err != nil {
 			return
 		}
 	}
@@ -83,126 +83,124 @@ func Init(fontByte,logoByte []byte, size int,fontSize float64,fontDPI float64 ,f
 	return
 }
 
+func (self *QrCode) GenerateQrCodeWithAlphaLogoArea(url string) (imgData []byte, err error) {
 
-func (self *QrCode)GenerateQrCodeWithAlphaLogoArea(url string)(imgData []byte,err error){
-
-	qrCodeData,err:=self.GenerateQRCode(url)
-	if err!=nil{
+	qrCodeData, err := self.GenerateQRCode(url)
+	if err != nil {
 		return
 	}
 
-	qrCodeImage,_,err:=image.Decode(bytes.NewReader(qrCodeData))
-	if err!=nil{
+	qrCodeImage, _, err := image.Decode(bytes.NewReader(qrCodeData))
+	if err != nil {
 		return
 	}
 
-
-	offsetX := (self.Size-200)/2
+	offsetX := (self.Size - 200) / 2
 
 	newImg := image.NewRGBA(qrCodeImage.Bounds())
 
 	//draw.Draw(newImg,newImg.Bounds(),&image.Uniform{self.BackColor}, image.Point{}, draw.Src)
-	draw.Draw(newImg,qrCodeImage.Bounds(),qrCodeImage,image.Point{-1,-1},draw.Src)
+	draw.Draw(newImg, qrCodeImage.Bounds(), qrCodeImage, image.Point{-1, -1}, draw.Src)
 
-	blank := image.NewRGBA(image.Rect(0,0,200,200))
+	blank := image.NewRGBA(image.Rect(0, 0, 200, 200))
 
-	draw.Draw(newImg, newImg.Bounds(), blank, image.Point{-offsetX,-offsetX}, draw.Src)
+	draw.Draw(newImg, newImg.Bounds(), blank, image.Point{-offsetX, -offsetX}, draw.Src)
 
-
-	buffer:=new(bytes.Buffer)
-	err=png.Encode(buffer,newImg)
-	if err!=nil{
+	buffer := new(bytes.Buffer)
+	err = png.Encode(buffer, newImg)
+	if err != nil {
 		return
 	}
-	imgData=buffer.Bytes()
+	imgData = buffer.Bytes()
 	return
-
-
-
 
 }
 
-func (self *QrCode)GenerateQRCodeWithLogoAndTitle(url,title string)(imgData []byte,err error)  {
+func (self *QrCode) GenerateQRCodeWithLogoAndTitle(url, title, titleColorHex string) (imgData []byte, err error) {
 
-	qrCodeData,err:=self.GenerateQRCodeWithLogo(url)
-	if err!=nil{
+	qrCodeData, err := self.GenerateQRCodeWithLogo(url)
+	if err != nil {
 		return
 	}
 
-	qrCodeImage,_,err:=image.Decode(bytes.NewReader(qrCodeData))
-	if err!=nil{
+	qrCodeImage, _, err := image.Decode(bytes.NewReader(qrCodeData))
+	if err != nil {
 		return
 	}
 
-
-
-	c:=freetype.NewContext()
+	c := freetype.NewContext()
 	c.SetDPI(self.FontDPI)
 	c.SetFont(self.Font)
 	c.SetFontSize(self.FontSize)
-	titleImage:=image.NewRGBA(image.Rect(0,0,self.Size,c.PointToFixed(self.FontSize).Round()*2))
+	titleImage := image.NewRGBA(image.Rect(0, 0, self.Size, c.PointToFixed(self.FontSize).Round()*2))
 	c.SetClip(titleImage.Bounds())
 	c.SetDst(titleImage)
-	c.SetSrc(image.Black)
-	startx:=1000-c.PointToFixed(40).Round()*utf8.RuneCountInString(title)-100
-	starty:=100-c.PointToFixed(40).Round()+10
-	position:=freetype.Pt(startx,starty) //字体出现的位置
-	_,err=c.DrawString(title,position)
-	if err!=nil{
+
+	var titleColor colorful.Color
+	if titleColorHex != "" {
+		titleColor, err = colorful.Hex(titleColorHex)
+		if err != nil {
+			return
+		}
+		c.SetSrc(image.NewUniform(titleColor))
+	} else {
+		c.SetSrc(image.Black)
+	}
+
+	startx := 1000 - c.PointToFixed(40).Round()*utf8.RuneCountInString(title) - 100
+	starty := 100 - c.PointToFixed(40).Round() + 10
+	position := freetype.Pt(startx, starty) //字体出现的位置
+	_, err = c.DrawString(title, position)
+	if err != nil {
 		return
 	}
 
 	newImg := image.NewRGBA(qrCodeImage.Bounds())
 
-	draw.Draw(newImg,newImg.Bounds(),&image.Uniform{color.White}, image.Point{}, draw.Src)
-	draw.Draw(newImg,newImg.Bounds(),qrCodeImage,image.Point{-1,-1}, draw.Over)
-	draw.Draw(newImg,newImg.Bounds(),titleImage,image.Point{-1,-1},draw.Over)
-	buffer:=new(bytes.Buffer)
-	err=png.Encode(buffer,newImg)
-	if err!=nil{
+	draw.Draw(newImg, newImg.Bounds(), &image.Uniform{color.White}, image.Point{}, draw.Src)
+	draw.Draw(newImg, newImg.Bounds(), qrCodeImage, image.Point{-1, -1}, draw.Over)
+	draw.Draw(newImg, newImg.Bounds(), titleImage, image.Point{-1, -1}, draw.Over)
+	buffer := new(bytes.Buffer)
+	err = png.Encode(buffer, newImg)
+	if err != nil {
 		return
 	}
-	imgData=buffer.Bytes()
+	imgData = buffer.Bytes()
 	return
-
 
 }
 
-func (self *QrCode)GenerateQRCodeWithLogo(url string)(imageData []byte,err error){
+func (self *QrCode) GenerateQRCodeWithLogo(url string) (imageData []byte, err error) {
 
-	qrCodeData,err:=self.GenerateQRCode(url)
-	if err!=nil{
+	qrCodeData, err := self.GenerateQRCode(url)
+	if err != nil {
 		return
 	}
 
-	qrCodeImage,_,err:=image.Decode(bytes.NewReader(qrCodeData))
-	if err!=nil{
+	qrCodeImage, _, err := image.Decode(bytes.NewReader(qrCodeData))
+	if err != nil {
 		return
 	}
 
-
-	offsetX := (self.Size-self.LogoImage.Bounds().Dx())/2
+	offsetX := (self.Size - self.LogoImage.Bounds().Dx()) / 2
 
 	newImg := image.NewRGBA(qrCodeImage.Bounds())
 
-	draw.Draw(newImg,newImg.Bounds(),&image.Uniform{self.BackColor}, image.Point{}, draw.Src)
-	draw.Draw(newImg,qrCodeImage.Bounds(),qrCodeImage,image.Point{-1,-1},draw.Over)
-	draw.Draw(newImg, newImg.Bounds(), self.LogoImage, image.Point{-offsetX,-offsetX}, draw.Over)
+	draw.Draw(newImg, newImg.Bounds(), &image.Uniform{self.BackColor}, image.Point{}, draw.Src)
+	draw.Draw(newImg, qrCodeImage.Bounds(), qrCodeImage, image.Point{-1, -1}, draw.Over)
+	draw.Draw(newImg, newImg.Bounds(), self.LogoImage, image.Point{-offsetX, -offsetX}, draw.Over)
 
-
-	buffer:=new(bytes.Buffer)
-	err=png.Encode(buffer,newImg)
-	if err!=nil{
+	buffer := new(bytes.Buffer)
+	err = png.Encode(buffer, newImg)
+	if err != nil {
 		return
 	}
-	imageData=buffer.Bytes()
+	imageData = buffer.Bytes()
 	return
-
-
 
 }
 
-func (self *QrCode)GenerateQRCode(url string)(imageData []byte,err error)  {
+func (self *QrCode) GenerateQRCode(url string) (imageData []byte, err error) {
 
 	q, e := qrcode.New(url, qrcode.High)
 	if e != nil {
@@ -212,25 +210,21 @@ func (self *QrCode)GenerateQRCode(url string)(imageData []byte,err error)  {
 	q.ForegroundColor = self.FrontColor
 	q.BackgroundColor = self.BackColor
 
-	imageData,err= q.PNG(self.Size)
+	imageData, err = q.PNG(self.Size)
 	return
 }
 
-
-
-func (self *QrCode)readFontFromByte(fontByte []byte)(err error){
-	self.Font,err=freetype.ParseFont(fontByte)
+func (self *QrCode) readFontFromByte(fontByte []byte) (err error) {
+	self.Font, err = freetype.ParseFont(fontByte)
 	return
 }
 
-func (self *QrCode)readLogoImageFromByte(logoByte []byte)(err error){
+func (self *QrCode) readLogoImageFromByte(logoByte []byte) (err error) {
 
-	logoImage,_,err:=image.Decode(bytes.NewReader(logoByte))
-	if err!=nil{
+	logoImage, _, err := image.Decode(bytes.NewReader(logoByte))
+	if err != nil {
 		return
 	}
-	self.LogoImage=resize.Resize(300,300,logoImage,resize.Lanczos3)
+	self.LogoImage = resize.Resize(300, 300, logoImage, resize.Lanczos3)
 	return
 }
-
-
--- qrcode_test.go
+++ qrcode_test.go
@@ -8,68 +8,66 @@ import (
 
 var qrCode *QrCode
 
-func init(){
-	logoData,err:=ioutil.ReadFile("logo.png")
-	fontData,err:=ioutil.ReadFile("zh.ttf")
-	if err!=nil{
+func init() {
+	logoData, err := ioutil.ReadFile("logo.png")
+	fontData, err := ioutil.ReadFile("zh.ttf")
+	if err != nil {
 		return
 	}
 
-
-	qrCode,err=Init(fontData,logoData,0,0,0,"","")
-	if err!=nil{
+	qrCode, err = Init(fontData, logoData, 0, 0, 0, "", "")
+	if err != nil {
 		panic(err)
 	}
 }
 
 func TestQrCode_GenerateQRCode(t *testing.T) {
-	pngData,err:=qrCode.GenerateQRCode("https://demo.u1200.com/af83b54a95b95ade250e334704ffddad7cb06fc9")
-	if err!=nil{
+	pngData, err := qrCode.GenerateQRCode("https://demo.u1200.com/af83b54a95b95ade250e334704ffddad7cb06fc9")
+	if err != nil {
 		t.Error(err)
 	}
 
-	file,err:=os.Create("qrcode.png")
-	if err!=nil{
+	file, err := os.Create("qrcode.png")
+	if err != nil {
 		t.Error(err)
 	}
 	file.Write(pngData)
 }
 
 func TestQrCode_GenerateQRCodeWithLogo(t *testing.T) {
-	pngData,err:=qrCode.GenerateQRCodeWithLogo("https://demo.u1200.com/af83b54a95b95ade250e334704ffddad7cb06fc9")
-	if err!=nil{
+	pngData, err := qrCode.GenerateQRCodeWithLogo("https://demo.u1200.com/af83b54a95b95ade250e334704ffddad7cb06fc9")
+	if err != nil {
 		t.Error(err)
 	}
 
-	file,err:=os.Create("qrcodeWithLogo.png")
-	if err!=nil{
+	file, err := os.Create("qrcodeWithLogo.png")
+	if err != nil {
 		t.Error(err)
 	}
 	file.Write(pngData)
 }
 
-
 func TestQrCode_GenerateQRCodeWithLogoAndTitle(t *testing.T) {
-	pngData,err:=qrCode.GenerateQRCodeWithLogoAndTitle("test","都看到看看地方")
-	if err!=nil{
+	pngData, err := qrCode.GenerateQRCodeWithLogoAndTitle("test", "都看到看看地方", "#f3f3f3")
+	if err != nil {
 		t.Error(err)
 	}
 
-	file,err:=os.Create("qrcodeWithLogoAndTitle.png")
-	if err!=nil{
+	file, err := os.Create("qrcodeWithLogoAndTitle.png")
+	if err != nil {
 		t.Error(err)
 	}
 	file.Write(pngData)
 }
 
 func TestQrCode_GenerateQrCodeWithAlphaLogoArea(t *testing.T) {
-	pngData,err:=qrCode.GenerateQrCodeWithAlphaLogoArea("test")
-	if err!=nil{
+	pngData, err := qrCode.GenerateQrCodeWithAlphaLogoArea("test")
+	if err != nil {
 		t.Error(err)
 	}
 
-	file,err:=os.Create("qrcodeWithAlpha.png")
-	if err!=nil{
+	file, err := os.Create("qrcodeWithAlpha.png")
+	if err != nil {
 		t.Error(err)
 	}
 	file.Write(pngData)
